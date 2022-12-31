0b2243fa63ffc35de8059159d638fb6b8ed72d6d
Hector <thehector1593@gmail.com>
2022-12-31 20:10:10 +0100

    chore: do not use any on include method

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
+import { App } from "vue";
 import NoticeCard from "./NoticeCard.vue";
 import RegularCard from "./RegularCard.vue";
 
 const NOTICE_CARD_TAG = "notice-card";
 const REGULAR_CARD_TAG = "regular-card";
 
-export default {
-  NoticeCard,
-  RegularCard,
-};
-
-export function include(app: any): any {
-  app.component(NOTICE_CARD_TAG, NoticeCard);
-  app.component(REGULAR_CARD_TAG, RegularCard);
+function include(app: App): App {
+  return app
+    .component(NOTICE_CARD_TAG, NoticeCard)
+    .component(REGULAR_CARD_TAG, RegularCard);
 }
+
+export default include;
+export { NoticeCard, RegularCard };
