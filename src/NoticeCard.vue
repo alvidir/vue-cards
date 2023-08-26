507ae9ece5a7b79cbc7b936162fa94f218b3b525
Hector <hector.morales.carnice@gmail.com>
2023-08-26 03:02:45 +0200

    chore(deps): merge dependabot branches

--- src/NoticeCard.vue
+++ src/NoticeCard.vue
@@ -1,136 +1,74 @@
-<template>
-  <div class="notice-card" v-if="active">
-    <button v-if="closable" class="close bx bx-x" @click="onClose"></button>
-    <div v-if="flagged" class="flag"></div>
-    <div v-else class="symbol">
-      <i :class="symbol"></i>
-    </div>
-    <div class="content">
-      <span v-if="title" :class="{ adjusted: closable }">{{ title }}</span>
-      <p v-if="text">{{ text }}</p>
-    </div>
-  </div>
-</template>
+<script setup lang="ts">
+import { computed } from "vue";
+import RegularCard from "./RegularCard.vue";
 
-<script lang="ts">
-import { defineComponent, PropType } from "vue";
-
-export enum NoticeLevel {
-  INFO = "info",
-  ERROR = "error",
+interface Props {
+  title?: string;
+  text?: string;
+  closeable?: boolean;
+  level?: string;
 }
 
-interface LevelData {
-  symbol: string;
-  color: string;
+const props = withDefaults(defineProps<Props>(), {
+  level: "info",
+});
+
+interface Events {
+  (e: "close", payload: MouseEvent): void;
 }
 
-const CLOSE_EVENT_NAME = "close";
-const LEVEL_DATA: { [key: string]: LevelData } = {
-  [NoticeLevel.INFO]: {
-    symbol: "bx bxs-info-circle",
-    color: "var(--color-accent)",
-  },
-  [NoticeLevel.ERROR]: {
-    symbol: "bx bxs-error",
-    color: "var(--color-red)",
-  },
-};
+const emit = defineEmits<Events>();
 
-export default defineComponent({
-  name: "NoticeCard",
-  emits: [CLOSE_EVENT_NAME],
-  components: {},
+const symbol = computed((): string => {
+  const symbols: { [key: string]: string } = {
+    info: "bx bxs-info-circle",
+    error: "bx bxs-error",
+  };
 
-  props: {
-    title: String,
-    text: String,
-    flagged: Boolean,
-    closable: Boolean,
-    level: {
-      type: String as PropType<NoticeLevel>,
-      default: NoticeLevel.INFO,
-    },
-    active: {
-      type: Boolean,
-      default: true,
-    },
-  },
+  return symbols[props.level];
+});
 
-  computed: {
-    color(): string {
-      return LEVEL_DATA[this.level].color;
-    },
+const color = computed((): string => {
+  const colors: { [key: string]: string } = {
+    info: "var(--color-accent)",
+    error: "var(--color-red)",
+  };
 
-    symbol(): string {
-      return LEVEL_DATA[this.level].symbol;
-    },
-  },
-
-  methods: {
-    onClose() {
-      this.$emit(CLOSE_EVENT_NAME);
-    },
-  },
+  return colors[props.level];
 });
 </script>
 
-<style lang="scss">
-@import "./styles.scss";
+<template>
+  <regular-card
+    class="notice-card"
+    :closeable="closeable"
+    @close="emit('close', $event)"
+  >
+    <template #header>
+      <span> <i :class="symbol"></i>&nbsp;{{ title }} </span>
+    </template>
 
+    <p v-if="text">{{ text }}</p>
+  </regular-card>
+</template>
+
+<style lang="scss">
+@import "fibonacci-styles";
 $base-color: unquote(v-bind(color));
 
 .notice-card {
-  @extend .round-corners, .fib-5;
-
-  display: flex;
-  position: relative;
-  flex-direction: row;
   border: 1px solid $base-color;
-  background: var(--color-bg-primary);
-  padding-right: $fib-5 * 1px;
-  overflow: hidden;
 
-  button {
+  & > button.close {
     color: $base-color !important;
   }
 
-  .flag {
-    width: $fib-4 * 1px;
-    background: $base-color;
-    border: 1px solid $base-color;
+  & > .header {
+    color: $base-color;
   }
 
-  .symbol {
-    color: $base-color;
-    padding: $fib-6 * 1px;
-    padding-right: 0;
-    font-weight: 600;
-  }
-
-  .content {
-    width: 100%;
-    padding: $fib-6 * 1px;
-    color: $base-color;
-    margin-top: auto;
-    margin-bottom: auto;
-
-    span {
-      font-weight: 600;
-      margin-right: $fib-5 * 1px;
-    }
-
-    p {
-      margin-top: $fib-6 * 1px;
-      margin-right: $fib-5 * 1px;
-    }
-  }
-
-  button {
-    background: none;
-    color: $base-color;
-    font-size: large;
-    border: none;
+  & > .body {
+    color: var(--color-text-secondary);
   }
 }
 </style>
